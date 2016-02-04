5cf0ad84cd904e15d22a5edfd50de2aea137fd62
Viktor Hanstorp <ndi14vhp@student.hig.se>
2016-02-04 14:58:30 +0100

    Add method headers

--- Programvaruteknik_Lab1/src/se/hig/programvaruteknik/DataSource.java
+++ Programvaruteknik_Lab1/src/se/hig/programvaruteknik/DataSource.java
@@ -1,6 +1,13 @@
 package se.hig.programvaruteknik;
 
+import java.time.LocalDate;
+import java.util.Map;
+
 public interface DataSource
 {
+    public String getName();
 
+    public String getUnit();
+
+    public Map<LocalDate, Double> getData();
 }
