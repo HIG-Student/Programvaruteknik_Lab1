5cc4c6b9980d0e753ea319fd32982fe0d3f27ce5
Viktor Hanstorp <ndi14vhp@student.hig.se>
2016-02-04 15:23:13 +0100

    Add content

--- Programvaruteknik_Lab1/src/se/hig/programvaruteknik/DataCollection.java
+++ Programvaruteknik_Lab1/src/se/hig/programvaruteknik/DataCollection.java
@@ -1,6 +1,37 @@
 package se.hig.programvaruteknik;
 
+import java.util.List;
+import java.util.Map;
+
 public class DataCollection
 {
+    private String title = null;
+    private DataSource xData;
+    private DataSource yData;
+    private Resolution resolution;
+    private Map<String, List<MatchedDataPair>> resultData;
+    private Map<String, MatchedDataPair> finalResult;
 
+    public DataCollection(DataSource xData, DataSource yData, Resolution resolution)
+    {
+	this.xData = xData;
+	this.yData = yData;
+	this.resolution = resolution;
+    }
+
+    public DataCollection setTitle(String title)
+    {
+	this.title = title;
+	return this;
+    }
+
+    public String getTitle()
+    {
+	return title == null ? (xData.getName() + " : " + yData.getName()) : title;
+    }
+
+    public DataCollectionBuilder getResult()
+    {
+	return null;// TODO: implement
+    }
 }
