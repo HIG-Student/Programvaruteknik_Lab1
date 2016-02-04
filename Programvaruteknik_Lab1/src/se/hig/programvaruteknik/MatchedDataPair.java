d106830cf9c125099a30445ea01410d6cb8326f5
Viktor Hanstorp <ndi14vhp@student.hig.se>
2016-02-04 15:02:15 +0100

    Add content

--- Programvaruteknik_Lab1/src/se/hig/programvaruteknik/MatchedDataPair.java
+++ Programvaruteknik_Lab1/src/se/hig/programvaruteknik/MatchedDataPair.java
@@ -2,5 +2,28 @@ package se.hig.programvaruteknik;
 
 public class MatchedDataPair
 {
+    private Double xValue;
+    private Double yValue;
 
+    public MatchedDataPair(Double xValue, Double yValue)
+    {
+	this.xValue = xValue;
+	this.yValue = yValue;
+    }
+
+    public Double getXValue()
+    {
+	return xValue;
+    }
+
+    public Double getYValue()
+    {
+	return yValue;
+    }
+
+    @Override
+    public String toString()
+    {
+	return "(" + xValue + " : " + yValue + ")";
+    }
 }
