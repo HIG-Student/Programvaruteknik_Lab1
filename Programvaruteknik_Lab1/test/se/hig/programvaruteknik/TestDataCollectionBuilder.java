7706faa937b688f971ec75edfa92e8ff49ef72e7
Viktor Hanstorp <ndi14vhp@student.hig.se>
2016-02-07 02:18:36 +0100

    Update
    Refactoring and adding more testcases

--- Programvaruteknik_Lab1/test/se/hig/programvaruteknik/TestDataCollectionBuilder.java
+++ Programvaruteknik_Lab1/test/se/hig/programvaruteknik/TestDataCollectionBuilder.java
@@ -6,6 +6,7 @@ import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
+import java.util.function.Consumer;
 
 import org.junit.Test;
 
@@ -71,105 +72,237 @@ public class TestDataCollectionBuilder
 		DataCollectionBuilder.localDateToString(LocalDate.of(2016, 1, 1), Resolution.YEAR));
     }
 
-    @Test
-    public void testCreate()
+    @SuppressWarnings("serial")
+    private static final LinkedList<LocalDate> dates = new LinkedList<LocalDate>()
     {
-	LinkedList<LocalDate> dates = new LinkedList<>();
-
-	for (int i = 1; i <= 20; i++)
-	    dates.add(LocalDate.of(2016, 2, i));
-	for (int i = 1; i <= 20; i++)
-	    dates.add(LocalDate.of(2016, 3, i));
-
-	DataSource sourceA = new DataSource()
 	{
+	    // YEAR
+	    {
+		// QUARTER
+		{
+		    // MONTH
+		    {
+			// WEEK
+			{
+			    for (int i = 1; i <= 7; i++)
+				add(LocalDate.of(2016, 2, i));
+			}
+			// WEEK
+			{
+			    for (int i = 8; i <= 14; i++)
+				add(LocalDate.of(2016, 2, i));
+			}
+			// WEEK
+			{
+			    for (int i = 15; i <= 21; i++)
+				add(LocalDate.of(2016, 2, i));
+			}
+		    }
+		    // MONTH
+		    {
+			// WEEK
+			{
+			    for (int i = 7; i <= 13; i++)
+				add(LocalDate.of(2016, 3, i));
+			}
+			// WEEK
+			{
+			    for (int i = 14; i <= 20; i++)
+				add(LocalDate.of(2016, 3, i));
+			}
+		    }
+		}
+	    }
 
+	    // YEAR
+	    {
+		// QUARTER
+		{
+		    // MONTH
+		    {
+			// WEEK
+			{
+			    for (int i = 2; i <= 8; i++)
+				add(LocalDate.of(2017, 1, i));
+			}
+		    }
+		}
+	    }
+	}
+    };
+
+    private DataSource getDataSource(String name, String unit, HashMap<LocalDate, Double> data)
+    {
+	return new DataSource()
+	{
 	    @Override
 	    public String getName()
 	    {
-		return "sourceA";
+		return name;
 	    }
 
 	    @Override
 	    public String getUnit()
 	    {
-		return "A";
+		return unit;
 	    }
 
-	    @SuppressWarnings("serial")
 	    @Override
 	    public Map<LocalDate, Double> getData()
 	    {
-		return new HashMap<LocalDate, Double>()
-		{
-		    {
-			for (LocalDate date : dates)
-			{
-			    put(date, 1d);
-			}
-		    }
-		};
+		return data;
 	    }
-
 	};
-
-	DataSource sourceB = new DataSource()
-	{
-
-	    @Override
-	    public String getName()
-	    {
-		return "sourceB";
-	    }
-
-	    @Override
-	    public String getUnit()
-	    {
-		return "B";
-	    }
-
-	    @SuppressWarnings("serial")
-	    @Override
-	    public Map<LocalDate, Double> getData()
-	    {
-		return new HashMap<LocalDate, Double>()
-		{
-		    {
-			for (LocalDate date : dates)
-			{
-			    put(date, 2d);
-			}
-		    }
-		};
-	    }
-
-	};
-
-	{
-	    DataCollection coll = new DataCollectionBuilder(sourceA, sourceB, Resolution.DAY).getResult();
-	    Map<String, MatchedDataPair> data = coll.getData();
-	    for (LocalDate date : dates)
-	    {
-		String key = DataCollectionBuilder.localDateToString(date, Resolution.DAY);
-		assertTrue("Missing key", data.containsKey(key));
-		MatchedDataPair pair = data.get(key);
-		assertEquals("Incorrect value", new Double(1), pair.getXValue());
-		assertEquals("Incorrect value", new Double(2), pair.getYValue());
-	    }
-	}
-
-	{
-	    DataCollection coll = new DataCollectionBuilder(sourceA, sourceB, Resolution.MONTH).getResult();
-	    Map<String, MatchedDataPair> data = coll.getData();
-
-	    assertEquals("Incorrect value", 2, data.size());
-	    assertEquals("Incorrect value", new Double(20), data.get("2016-02").getXValue());
-	    assertEquals("Incorrect value", new Double(40), data.get("2016-02").getYValue());
-
-	    assertEquals("Incorrect value", new Double(20), data.get("2016-03").getXValue());
-	    assertEquals("Incorrect value", new Double(40), data.get("2016-03").getYValue());
-	}
-
     }
 
+    @SuppressWarnings("serial")
+    private DataSource getDataSourceA()
+    {
+	return getDataSource("sourceA", "A", new HashMap<LocalDate, Double>()
+	{
+	    {
+		for (LocalDate date : dates)
+		{
+		    put(date, 1d);
+		}
+	    }
+	});
+    }
+
+    @SuppressWarnings("serial")
+    private DataSource getDataSourceB()
+    {
+	return getDataSource("sourceB", "B", new HashMap<LocalDate, Double>()
+	{
+	    {
+		for (LocalDate date : dates)
+		{
+		    put(date, 2d);
+		}
+	    }
+	});
+    }
+
+    private DataCollection createCollection(Resolution resolution)
+    {
+	return new DataCollectionBuilder(getDataSourceA(), getDataSourceB(), resolution).getResult();
+    }
+
+    private interface testCollectionCase
+    {
+	public void test(LocalDate date, Double x, Double y);
+    }
+
+    private void testCollection(Resolution resolution, testCollectionCase testCase)
+    {
+	DataCollection coll = createCollection(resolution);
+	Map<String, MatchedDataPair> data = coll.getData();
+	for (LocalDate date : dates)
+	{
+	    String key = DataCollectionBuilder.localDateToString(date, resolution);
+	    assertTrue("Missing key", data.containsKey(key));
+	    MatchedDataPair pair = data.get(key);
+	    testCase.test(date, pair.getXValue(), pair.getYValue());
+	}
+    }
+
+    @Test
+    public void testTitle()
+    {
+	testCollection(Resolution.DAY, (date, x, y) ->
+	{
+	    assertEquals("Incorrect value", new Double(1), x);
+	    assertEquals("Incorrect value", new Double(2), y);
+	});
+    }
+
+    @Test
+    public void testDay()
+    {
+	testCollection(Resolution.DAY, (date, x, y) ->
+	{
+	    assertEquals("Incorrect value", new Double(1), x);
+	    assertEquals("Incorrect value", new Double(2), y);
+	});
+    }
+
+    @Test
+    public void testWeek()
+    {
+	testCollection(Resolution.WEEK, (date, x, y) ->
+	{
+	    assertEquals("Incorrect value", new Double(7), x);
+	    assertEquals("Incorrect value", new Double(14), y);
+	});
+    }
+
+    @Test
+    public void testMonth()
+    {
+	testCollection(Resolution.MONTH, (date, x, y) ->
+	{
+	    switch (date.getMonth())
+	    {
+	    case JANUARY:
+		assertEquals(new Double(7), x);
+		assertEquals(new Double(14), y);
+		break;
+	    case FEBRUARY:
+		assertEquals(new Double(21), x);
+		assertEquals(new Double(42), y);
+		break;
+	    case MARCH:
+		assertEquals(new Double(14), x);
+		assertEquals(new Double(28), y);
+		break;
+	    default:
+		fail("Unexpected month");
+		break;
+	    }
+	});
+    }
+
+    @Test
+    public void testQuarter()
+    {
+	testCollection(Resolution.QUARTER, (date, x, y) ->
+	{
+	    switch (date.getYear())
+	    {
+	    case 2016:
+		assertEquals(new Double(35), x);
+		assertEquals(new Double(70), y);
+		break;
+	    case 2017:
+		assertEquals(new Double(7), x);
+		assertEquals(new Double(14), y);
+		break;
+	    default:
+		fail("Unexpected year");
+		break;
+	    }
+	});
+    }
+
+    @Test
+    public void testYear()
+    {
+	testCollection(Resolution.YEAR, (date, x, y) ->
+	{
+	    switch (date.getYear())
+	    {
+	    case 2016:
+		assertEquals(new Double(35), x);
+		assertEquals(new Double(70), y);
+		break;
+	    case 2017:
+		assertEquals(new Double(7), x);
+		assertEquals(new Double(14), y);
+		break;
+	    default:
+		fail("Unexpected year");
+		break;
+	    }
+	});
+    }
 }
